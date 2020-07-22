a38b773ebcb093e819ae5c45e8b120d4903066df
Shivansh Srivastava <shivanshsrivastava2000@gmail.com>
2020-07-22 16:04:13 +0530

    Implementing filters : Query Class

--- shortener/schema.py
+++ shortener/schema.py
@@ -11,10 +11,16 @@ class URLType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    urls = graphene.List(URLType)
+    urls = graphene.List(URLType, url=graphene.String())
 
-    def resolve_urls(self, info, **kwargs):
-        return URL.objects.all()
+    def resolve_urls(self, info, url=None, **kwargs):
+        queryset = URL.objects.all()
+
+        if url:
+            _filter = Q(full_url__icontains=url)
+            queryset = queryset.filter(_filter)
+
+        return queryset
 
 
 class CreateURL(graphene.Mutation):
@@ -23,7 +29,7 @@ class CreateURL(graphene.Mutation):
     class Arguments:
         full_url = graphene.String()
 
-    def mutate(self, info, full_url):
+    def mutate(self, info, full_url)
         url = URL(full_url=full_url)
         url.save()
 
